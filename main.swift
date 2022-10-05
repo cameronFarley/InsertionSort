24fd2949ebd73616838eb8e116ca068db86a0e7e
Cameron Farley <cameron.farley@student.allenisd.org>
2022-10-05 16:55:53 -0500

    removed print statement

--- main.swift
+++ main.swift
@@ -10,23 +10,14 @@ let unsortedIntegers = [5, 1, 4, 2, 8]
 let STDIN = (readLine(strippingNewline: true))?.split {$0 == " "}.map (String.init)
 
 func insertionSort(_ array: [String]) {
-    var pass = 0
-    var swapTotal = 0
-    var swaps = 0
     var a = array                             
     for x in 1..<a.count {                    
         var y = x
         while y > 0 && a[y] < a[y - 1] {
             (a[y], a[y-1]) = (a[y-1], a[y])
-            y -= 1
-            swapTotal += 1
-            swaps += 1
+            y -= 1           
         }
-        pass += 1
-        print("Pass: \(pass), Swaps: \(swaps)/\(swapTotal), Array: \(a)")
-        swaps = 0
     }
 }
 
-print("Pass: 0, Swaps: 0/0, Array: \(STDIN!)")
-insertionSort(STDIN!)
+print(insertionSort(STDIN!))
